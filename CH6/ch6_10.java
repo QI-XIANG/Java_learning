b91eac6e14943eb0a50ac54d197e718b11fa69dd
qixiang <qixiang1009@gmail.com>
2024-05-08 13:13:40 +0800

    add content

---
+++ CH6/ch6_10.java
@@ -0,0 +1,26 @@
+package CH6;
+
+public class ch6_10 {
+    public static void main(String [] args){
+        //calculate euler number
+        double e = 1;
+        int n = 1;
+        double item = 1;
+        while(n <= 100){
+            item = 1.0/factorial(n);
+            e += item;
+            if(n%10 == 0){
+                System.out.printf("當 i = %d, e = %.14f\n", n, e);
+            }
+            n++;
+        }
+    } 
+    
+    public static double factorial(int n){
+        double result = 1;
+        for(int i=1; i<=n; i++){
+            result *= i;
+        }
+        return result;
+    }
+}

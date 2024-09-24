50485fe15a0187a67ecf3f175895a564ecd0bd89
qixiang <qixiang1009@gmail.com>
2024-09-24 23:59:10 +0800

    add content

---
+++ CH20/ch20_1.java
@@ -0,0 +1,12 @@
+package CH20;
+
+public class ch20_1 {
+    public static void main(String [] args){
+        int [] x = null;
+        int sum = 0;
+        for(int i = 0; i <= x.length; i++){
+            sum += x[i];
+            System.out.printf("索引值: %d , 加總結果: %d\n", i, sum);
+        }
+    }    
+}
